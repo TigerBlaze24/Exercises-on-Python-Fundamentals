5929aab43ca9ac6ad6c40c52bbf490f23994c4fa
TigerBlaze24 <cuanbellina@gmail.com>
2024-03-12 12:13:13 +0100

    colore.py

---
+++ esercizio3.py
@@ -0,0 +1,17 @@
+#scriverre una funzion colore casuale()che torna una stringa
+#casuale tra "rosso""verde "blu""arancio "ciano
+import random
+
+def colorecasuale():
+   colori =["rosso","giallo","verde","blu","arancio","ciano","rosa","turchese"]
+   return colori[random.randint(0, len(colori)-1)]
+
+print(colorecasuale())
+print(colorecasuale())
+print(colorecasuale())
+print(colorecasuale())
+    
+
+#stabilisce qual'è il numero mafgggiore:
+print(max(10,20))
+
