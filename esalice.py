a36ab8b6c1d1dc480988051fc2e29b62c066ec9e
TigerBlaze24 <cuanbellina@gmail.com>
2024-03-14 18:57:59 +0100

    esalice.py

--- esalice.py
+++ esalice.py
@@ -1,6 +1,6 @@
 fin = open("alice.txt", "r")
 linee = fin.readlines()  #legge tutte le linee
-fin.close()
+fin.close() #con questo comando si chiude il file
 
 l1=[]
  
@@ -12,4 +12,57 @@ for l in linee:
 linee=l1
 print(linee)
 
+#esercizio con split
+s = "alfa;beta;gamma"
+print(s.split(";"))
 
+#dato alice.txt, stampare, una per riga,tutte le parole che la compongono:
+fin = open("alice.txt", "r") #fin.(funzione che trova un file) open(apre il file)
+linee = fin.readlines()
+
+parole =[]
+
+for l in linee:
+    parole.extend(l.split(" "))
+
+print(parole)
+
+#data una lista di stringhe , eliminare nella lista tutte le stringhe vuote
+
+ls = ["uno","","due", "tre", "", " ", "", "fine"]
+
+lnuova=[]
+
+for i in ls:
+    
+    if len (i.strip()) >0:
+        lnuova.append(i)
+
+print(lnuova)
+
+
+#contare quanti caratteri ci sono in alice.txt e poi contare quanti caratteri con spazi bianchi
+#infine contare quanti caratteri alfanumerici[a-2A-z0-9]ci sono in alice. txt
+
+fin = open ("alice.txt", "r")
+linee = fin.readlines()
+fin.close()
+
+tot=0
+
+for linea in linee:
+    tot = tot + len (linea)
+
+print("caratteri: ",tot)
+
+#2)
+
+fin = open("alice.txt" , "r")
+alice=fin.read()
+fin.close()
+notblank=0
+for c in alice:
+    if c!=" ":
+        notblank += 1
+
+print("notblank ",tot)
---
+++ eserciziocambio.py
@@ -0,0 +1,9 @@
+#Data una strainga numerica"98123"covertirla in una lista di digit[9, 8 ,1 ,2, 3, ]
+
+import math
+def stringdigitstolist(sd):
+    lista =[]
+
+for i in:
+        lista
+    
