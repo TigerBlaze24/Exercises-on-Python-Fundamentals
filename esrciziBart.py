ee1e56e945c0ff2c52a75d8053ec5045b49bb654
TigerBlaze24 <cuanbellina@gmail.com>
2024-04-17 18:14:50 +0200

    eserciziBart.py

---
+++ esrciziBart.py
@@ -0,0 +1,52 @@
+from tkinter import Menu
+
+
+menu:dict={"pasta":10.50 ,"pizza":9.00 ,"salad":6.50 , "weine":4.00 ,"water":2.30} # type: ignore
+pasta_price = menu["pasta"]
+weine_price = menu["weine"]
+print (pasta_price)
+print(weine_price)
+
+
+#2.3)usa una variabile per rappresentare un nome di una persona, stampa il messaggio della persona
+nome = "Mary"
+print("hello what you like?")
+#2.4)
+nome_x = "luca"
+print("luca")
+print("LUCA")
+print("luca LUCA luca")
+
+#2.5)
+autore :str='Giuliocesare'
+frase = "quoue tu bruts fili mi"
+print(frase)
+
+#2.6)
+famousperson:str="famousperson"
+message = "piange il telefono"
+print(message)
+
+#2.8)
+print(filename.removesuffix(".txt"))
+
+#3.2)
+nomi =["luca","elena","gino" , "giuseppe"]
+print(nomi[0])
+print(nomi[1])
+print(nomi[2])
+print(nomi[3])
+print(nomi[4])
+
+#3.3)
+print("ciao best " , nomi[0] , "come stai?")
+print("ciao tata", nomi [1] ,"stai andado a casa?")
+print("ciao bello" , nomi [2] , "vieni al cinema?")
+print("ciao splendore" , nomi [3] , "vieni dal paninaro stasera?")
+print("ciao bellssimo" , nomi [4] , "con me dal parrucchiere?")
+
+
+
+
+
+
