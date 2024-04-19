dd6964ffe32b562e395cacea59072bfc2af7eccd
TigerBlaze24 <cuanbellina@gmail.com>
2024-04-19 16:27:16 +0200

    abbiamo provato le funzioni con questo esercizio

---
+++ ProjectVscode/esercizi/lezione2/esrciziBartclasse.py
@@ -0,0 +1,8 @@
+def area_rettangolo (x: float , y: float) -> float:
+    area = x*y
+    return area
+
+x, y = 2, 3
+output:float =area_rettangolo(2,3)
+print(f"l'area del reattangolo con x ={x} e y={y} è {output}")
+#questo esercizio è un esempio di funzione
