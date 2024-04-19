6e2a2d1e2e468931d89f931da2c4839ae5f1004d
TigerBlaze24 <cuanbellina@gmail.com>
2024-04-19 16:31:15 +0200

    esercizi bart in classe

--- esrciziBart.py
+++ esrciziBart.py
@@ -1,18 +1,26 @@
-menu:dict={"pasta":10.50 ,"pizza":9.00 ,"salad":6.50 , "weine":4.00 ,"water":2.30} # type: ignore
-pasta_price = menu["pasta"]
-weine_price = menu["weine"]
-print (pasta_price)
-print(weine_price)
+#Maria Paola Bongiorno
+#18/04/2024
 
 
 #2.3)usa una variabile per rappresentare un nome di una persona, stampa il messaggio della persona
-nome = "Mary"
-print("hello what you like?")
+name :str = "Mary"
+print("ciao ti và di mparare un pò di python insieme?")
+#oppurecosì in cui la variabile contiene il messaggio
+message: str = f"ciao{name}, ti và di imparare un pò di python insieme?"
+
 #2.4)
-nome_x = "luca"
-print("luca")
-print("LUCA")
-print("luca LUCA luca")
+#questa variabile contiene il nome di una persona
+name: str ="luca"
+
+#questa variabile contiene il nome in minuscolo
+name_lower: str = name.lower()
+
+#questa variabile contiene il nome in maiuscolo
+name_upper: str = name.upper()
+
+print( f" {name}, {name.lower} ,{name.upper}")
+
+
 
 #2.5)
 autore :str='Giuliocesare'
@@ -25,23 +33,47 @@ message = "piange il telefono"
 print(message)
 
 #2.8)
+filename:str ="prova.txt"
 print(filename.removesuffix(".txt"))
 
+#3.1)
+name =["luca","elena","gino" , "giuseppe"]
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
 #3.2)
-nomi =["luca","elena","gino" , "giuseppe"]
-print(nomi[0])
-print(nomi[1])
-print(nomi[2])
-print(nomi[3])
-print(nomi[4])
+print("ciao best " , name[0] , "come stai?")
+print("ciao tata", name[1] ,"stai andado a casa?")
+print("ciao bello" , name[2] , "vieni al cinema?")
+print("ciao splendore" , name[3] , "vieni dal paninaro stasera?")
 
 #3.3)
-print("ciao best " , nomi[0] , "come stai?")
-print("ciao tata", nomi [1] ,"stai andado a casa?")
-print("ciao bello" , nomi [2] , "vieni al cinema?")
-print("ciao splendore" , nomi [3] , "vieni dal paninaro stasera?")
-print("ciao bellssimo" , nomi [4] , "con me dal parrucchiere?")
-#da finire e poi scriverli
+car :list =["Mercedes","Ferrari","Lamborghini","Maserati"]
+message :list= ["vorreiguidare una", "mi piacerebbe avere una","mi piacerebbe comprarne una","desidero una"]
+print(message[0] , car[0])
+
+#3.4)
+guests : list =["Terenzio" ,"Fabrizio" , "Annibale", "Marco"]
+for name in guests :
+    print(name, " vieni a cena?")
+
+
+#3.5)
+print("Terenzio")
+guests[0]= "Tommaso"
+
+for name in guests :
+    print(name, " vieni a cena?")
+
+#3.6)
+print("ragazzi ho trovato un tavolo più grande")
+guests.insert(0, "Cesare")
+guests.insert(2, "Nerone")
+guests.append("Caio")
+
+print(guests)
 
 
 
